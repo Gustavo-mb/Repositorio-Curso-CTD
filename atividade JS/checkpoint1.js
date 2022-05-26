f7e1ad1228a32ae197653ba8a108501312a666be
Gustavo <moraesgb11@hotmail.com>
2022-05-25 22:38:42 -0300

    checkpoint js

---
+++ atividade JS/checkpoint1.js
@@ -0,0 +1,46 @@
+readlineSync = require('readline-sync');
+
+console.log(`ola lucas, qual prato vamos fazer hoje:
+
+1-pipoca     (preparo em 10)
+2-macarrao   (preparo em 8)
+3-carne      (preparo em 15)
+4-feijao     (preparo em 12)
+5-brigadeiro (preparo em 8)        `) 
+  
+let pedido = readlineSync.question(`escolha uma opcao acima `)
+
+if(pedido === '1'){
+    var tempopd =10
+    console.log(`voce selecionou pipoca`)
+}else if (pedido === '2'){
+    var tempopd =8
+    console.log(`voce selecionou macarrao`)
+}else if (pedido === '3'){
+    var tempopd =15
+    console.log(`voce selecionou carne`)
+}else if (pedido === '4'){
+    var tempopd =12
+    console.log(`voce selecionou feijao`)
+}else if (pedido ==='5'){
+    var tempopd =8
+    console.log(`voce selecionou brigadeiro`)
+}else {
+    console.log(`prato inexistente`)
+} 
+
+let preparo= readlineSync.question(`por quanto tempo deseja prepara-lo? `) 
+
+function micro (tempopd,preparo){
+
+if(preparo < tempopd ) {                    
+    console.log(`tempo insuficiente`)
+}else if (preparo <= tempopd*2){
+    console.log(`Prato pronto, bom apetite!!!`)
+}else if (preparo <= tempopd*3){
+    console.log(`a comida queimou`)
+}else console.log (`kabumm`)
+}    
+
+micro (tempopd,preparo)
+
