9caad152ffc31cc8fb9d9c853fbb8cbd7633dcec
Gustavo <moraesgb11@hotmail.com>
2023-05-14 22:57:06 -0300

    react no vite

---
+++ React atividade 2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Fragment } from 'react'
+
+function App() {
+  return (
+    <Fragment>
+    <a href="https://code.org/helloworld" target="_blank"><h1>Hello World!!</h1></a>
+    <h3>React Vite</h3>
+   </Fragment>
+    )
+  }
+
+export default App
