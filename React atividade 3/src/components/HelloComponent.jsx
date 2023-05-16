8031e107a2e6c352929560da48b0873cedc4f320
Gustavo <moraesgb11@hotmail.com>
2023-05-16 18:01:03 -0300

    manipulando componentes reacts

---
+++ React atividade 3/src/components/HelloComponent.jsx
@@ -0,0 +1,10 @@
+function Ola(prop) {
+  return (
+    <>
+      <p>Aula de {prop.materia}</p>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default Ola;
---
+++ React atividade 3/src/components/NomeComponent.jsx
@@ -0,0 +1,10 @@
+function Nome(prop) {
+  return (
+    <>
+      {prop.children}
+      <h1>{prop.nome}</h1>
+    </>
+  );
+}
+
+export default Nome;
